0fb9b38a43fa4aa01025b4b7202f4c47826cf30e
ranjith1807 <v.ranjith9095490240@gmail.com>
2026-03-29 12:32:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function convertToRoman(num) {
-  	const romanMatrix = [
+  const romanMatrix = [
     ['M', 1000], ['CM', 900], ['D', 500], ['CD', 400],
     ['C', 100],  ['XC', 90],  ['L', 50],  ['XL', 40],
     ['X', 10],   ['IX', 9],   ['V', 5],   ['IV', 4],
@@ -21,15 +21,8 @@ function convertToRoman(num) {
   return result;
 }
 
-  //your code here
-
-}
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
 // console.log(convertToRoman(36));
 
-
-
-
 // do not edit below this line
-module.exports = convertToRoman
+module.exports = convertToRoman;
